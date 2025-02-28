d8ef2a76549e0ae615bc339863e51ca2e1c9c3d1
KarolannMauger <karolannmauger@gmail.com>
2025-02-28 14:36:32 -0500

    add capteur ultrasonic

--- client/main.py
+++ client/main.py
@@ -1,22 +1,33 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from time import sleep
+from capteur_ultrasonic import distance
+from pigpio import pi, OUTPUT, INPUT
 
+SLEEP: int = 0.10
 PORT: int = 9090
-ADDR = ""
+ADDR: str = "10.10.21.138"
+TRIG: int = 15
+ECHO: int = 14
 
 if __name__ == "__main__":
-    try: 
+    pi = pi()
+
+    pi.set_mode(TRIG, OUTPUT)
+    pi.set_mode(ECHO, INPUT)
+    try:
         sock: socket = socket(AF_INET, SOCK_DGRAM)
         
-        dest_addr = (ADDR, PORT)
+        dest_addr: tuple = (ADDR, PORT)
         
         while True:
-            sock.send("".encode(), dest_addr)
-            sleep(0.5)
+            sock.sendto(str(distance(pi, TRIG, ECHO)).encode(), dest_addr)
+            sleep(SLEEP)
             
 
-    except Exception as error:
+    except (Exception | KeyboardInterrupt) as error:
         print(error)
         
     finally:
+        pi.stop()
         sock.close()
+        
