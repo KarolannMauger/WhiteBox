1b69bc546b0aeb8e5d8c9ee77ef349522c9e9ac1
KarolannMauger <karolannmauger@gmail.com>
2025-03-10 12:36:56 -0400

    Add comments, works, need median

--- serveur/main.py
+++ serveur/main.py
@@ -1,22 +1,32 @@
 from socket import socket, AF_INET, SOCK_DGRAM
-import test_led_couleur
+import led_color_control
 import queue
 import threading
 
+#Constants
 PORT: int = 9090
 BUFFER_SIZE: int = 1024
 
+#Create a queue to store the data
 data_queue = queue.Queue()
 
+#Create a socket to receive the messages from the client UDP
 socket_local: socket = socket(AF_INET, SOCK_DGRAM)
 adr_local: tuple = ("", PORT)
 socket_local.bind(adr_local)
 print(f"On attend les messages UDP entrants au port {PORT}...\n")
-                
-couleurs_proxemie = test_led_couleur.charger_couleurs_json()
+ 
+
+#Load the colors from the json file               
+couleurs_proxemie = led_color_control.charger_couleurs_json()
 
 
+#This thread opens the LED strip
+led_thread = threading.Thread(target=led_color_control.openLed, args=(data_queue, couleurs_proxemie), daemon=True).start()
 
+"""
+    This function receives the messages from the client UDP
+"""
 def msg_from_udp():
     while True:
         buffer, adr_local = socket_local.recvfrom(BUFFER_SIZE)
@@ -28,17 +38,14 @@ def msg_from_udp():
 
         print(f"> {msg}\n", end="")
 
-
-led_thread = threading.Thread(target=test_led_couleur.openLed, args=(data_queue, couleurs_proxemie), daemon=True).start()
-
-
-
+"""
+    This is the main function
+"""
 if __name__ == "__main__":
     try:
-        #data = data_queue.get_nowait()
         msg_from_udp()
     except KeyboardInterrupt:
         print("\nFin du programme")
     finally:
-        test_led_couleur.close_led()
+        led_color_control.close_led()
         socket_local.close()
