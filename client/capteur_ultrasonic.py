a997fdc519a1dffdea6bf0cde9d989efb6c4b4ed
KarolannMauger <karolannmauger@gmail.com>
2025-03-24 10:28:26 -0400

    Add median

--- client/capteur_ultrasonic.py
+++ client/capteur_ultrasonic.py
@@ -4,26 +4,26 @@ from time import time, sleep
     
 def distance(pi: pi, gpio_trigger: int, gpio_echo: int) -> int:
     try:
-        # Envoyer un son
+        # Send a sound
         pi.write(gpio_trigger, 1)
         sleep(0.00001)
         pi.write(gpio_trigger, 0)
 
-        # Attendre l'echo
+        # Wait for the echo
         debut = time()
         while pi.read(gpio_echo) == 0:
-            # Break si on attend le debut de l'echo plus que 1 seconde
+            # Break if we wait for the beginning of the echo more than 1 second
             if time() - debut > 1: 
                 break
 
-        # Echo arrive
+        # Echo arrives
         echo = time()
         while pi.read(gpio_echo) == 1:
-            # Break si on attend la fin de l'écho plus que 1 seconde
+            # Break if we wait for the end of the echo more than 1 second
             if time() - echo > 1: 
                 break
 
-        # Duree de l'echo
+        # Echo duration
         duree = time() - echo
         
         return int((duree * 34300) / 2)
